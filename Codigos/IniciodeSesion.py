ce837bedf40d170bdeb2b4a9b776bb30968e4e36
Daniel Sepulveda <daniel-thorney@hotmail.com>
2023-05-31 19:52:30 -0400

    Update IniciodeSesion.py

--- Codigos/IniciodeSesion.py
+++ Codigos/IniciodeSesion.py
@@ -1,3 +1,4 @@
+import sys
 from PyQt6 import QtCore, QtGui, QtWidgets
 
 
@@ -5,6 +6,7 @@ class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(265, 307)
+        
         self.label = QtWidgets.QLabel(parent=Form)
         self.label.setGeometry(QtCore.QRect(20, 10, 161, 21))
         font = QtGui.QFont()
@@ -12,9 +14,11 @@ class Ui_Form(object):
         font.setPointSize(12)
         self.label.setFont(font)
         self.label.setObjectName("label")
+        
         self.widget = QtWidgets.QWidget(parent=Form)
         self.widget.setGeometry(QtCore.QRect(10, 40, 241, 261))
         self.widget.setObjectName("widget")
+        
         self.label_2 = QtWidgets.QLabel(parent=self.widget)
         self.label_2.setGeometry(QtCore.QRect(10, 10, 49, 16))
         font = QtGui.QFont()
@@ -22,6 +26,7 @@ class Ui_Form(object):
         font.setPointSize(10)
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
+        
         self.label_3 = QtWidgets.QLabel(parent=self.widget)
         self.label_3.setGeometry(QtCore.QRect(10, 60, 71, 16))
         font = QtGui.QFont()
@@ -29,13 +34,16 @@ class Ui_Form(object):
         font.setPointSize(10)
         self.label_3.setFont(font)
         self.label_3.setObjectName("label_3")
+        
         self.pushButton = QtWidgets.QPushButton(parent=self.widget)
         self.pushButton.setGeometry(QtCore.QRect(10, 190, 75, 24))
         self.pushButton.setObjectName("pushButton")
+        
         self.lineEdit_5 = QtWidgets.QLineEdit(parent=self.widget)
         self.lineEdit_5.setGeometry(QtCore.QRect(10, 80, 191, 21))
         self.lineEdit_5.setText("")
         self.lineEdit_5.setObjectName("lineEdit_5")
+        
         self.lineEdit_4 = QtWidgets.QLineEdit(parent=Form)
         self.lineEdit_4.setGeometry(QtCore.QRect(20, 70, 191, 21))
         self.lineEdit_4.setText("")
@@ -54,7 +62,6 @@ class Ui_Form(object):
 
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
     ui = Ui_Form()
